f3b2b9b106b344f4ece69480694145d16e6f7f14
Marcos Junior <bw.marcos@gmail.com>
2014-06-08 10:32:34 -0300

    Primeiro atualiza

---
+++ Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn_baixar = New System.Windows.Forms.Button()
+        Me.barra = New System.Windows.Forms.ProgressBar()
+        Me.lista = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txt_caminho = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.lbl_valor = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.btn_mediworks = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.pasta_dialog = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_baixar
+        '
+        Me.btn_baixar.Location = New System.Drawing.Point(113, 249)
+        Me.btn_baixar.Name = "btn_baixar"
+        Me.btn_baixar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_baixar.TabIndex = 0
+        Me.btn_baixar.Text = "Baixar"
+        Me.btn_baixar.UseVisualStyleBackColor = True
+        '
+        'barra
+        '
+        Me.barra.Location = New System.Drawing.Point(113, 222)
+        Me.barra.Name = "barra"
+        Me.barra.Size = New System.Drawing.Size(299, 10)
+        Me.barra.TabIndex = 3
+        '
+        'lista
+        '
+        Me.lista.FormattingEnabled = True
+        Me.lista.Location = New System.Drawing.Point(113, 12)
+        Me.lista.Name = "lista"
+        Me.lista.Size = New System.Drawing.Size(298, 121)
+        Me.lista.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(194, 249)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Extrair"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txt_caminho
+        '
+        Me.txt_caminho.Location = New System.Drawing.Point(113, 163)
+        Me.txt_caminho.Name = "txt_caminho"
+        Me.txt_caminho.Size = New System.Drawing.Size(273, 20)
+        Me.txt_caminho.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(110, 147)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Caminho da Pasta:"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(387, 162)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(24, 22)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "..."
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ImageStream = CType(resources.GetObject("ImageList1.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList1.Images.SetKeyName(0, "works.png")
+        Me.ImageList1.Images.SetKeyName(1, "center.png")
+        Me.ImageList1.Images.SetKeyName(2, "capture.png")
+        Me.ImageList1.Images.SetKeyName(3, "remote.png")
+        '
+        'lbl_valor
+        '
+        Me.lbl_valor.AutoSize = True
+        Me.lbl_valor.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_valor.ForeColor = System.Drawing.Color.Black
+        Me.lbl_valor.Location = New System.Drawing.Point(109, 206)
+        Me.lbl_valor.Name = "lbl_valor"
+        Me.lbl_valor.Size = New System.Drawing.Size(52, 13)
+        Me.lbl_valor.TabIndex = 15
+        Me.lbl_valor.Text = "Tamanho"
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.White
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button7.ImageIndex = 2
+        Me.Button7.ImageList = Me.ImageList1
+        Me.Button7.Location = New System.Drawing.Point(3, 210)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(97, 63)
+        Me.Button7.TabIndex = 12
+        Me.Button7.Text = "MediCapture"
+        Me.Button7.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.White
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button6.ImageIndex = 3
+        Me.Button6.ImageList = Me.ImageList1
+        Me.Button6.Location = New System.Drawing.Point(3, 144)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(97, 63)
+        Me.Button6.TabIndex = 11
+        Me.Button6.Text = "MediRemote"
+        Me.Button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.White
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button5.ImageIndex = 1
+        Me.Button5.ImageList = Me.ImageList1
+        Me.Button5.Location = New System.Drawing.Point(3, 78)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(97, 63)
+        Me.Button5.TabIndex = 10
+        Me.Button5.Text = "MediCenter"
+        Me.Button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'btn_mediworks
+        '
+        Me.btn_mediworks.BackColor = System.Drawing.Color.White
+        Me.btn_mediworks.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_mediworks.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btn_mediworks.ImageIndex = 0
+        Me.btn_mediworks.ImageList = Me.ImageList1
+        Me.btn_mediworks.Location = New System.Drawing.Point(3, 12)
+        Me.btn_mediworks.Name = "btn_mediworks"
+        Me.btn_mediworks.Size = New System.Drawing.Size(97, 63)
+        Me.btn_mediworks.TabIndex = 9
+        Me.btn_mediworks.Text = "MediWorks"
+        Me.btn_mediworks.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_mediworks.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(104, 284)
+        Me.Panel1.TabIndex = 16
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(311, 238)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 284)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbl_valor)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.btn_mediworks)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_caminho)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.lista)
+        Me.Controls.Add(Me.barra)
+        Me.Controls.Add(Me.btn_baixar)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(445, 322)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "System UpDate"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_baixar As System.Windows.Forms.Button
+    Friend WithEvents barra As System.Windows.Forms.ProgressBar
+    Friend WithEvents lista As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txt_caminho As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btn_mediworks As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents ImageList1 As System.Windows.Forms.ImageList
+    Friend WithEvents lbl_valor As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents pasta_dialog As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,212 @@
+﻿Imports System.Net
+
+
+Imports System.Net.Dns
+Public Class Form1
+    Dim arquivo As String
+    Dim url_intall As String
+    Dim caminho As String
+
+    WithEvents webclient1 As New WebClient
+    WithEvents webclient2 As New WebClient
+
+
+    Private Sub btn_baixar_Click(sender As Object, e As EventArgs) Handles btn_baixar.Click
+
+        webclient2.DownloadFileAsync(New Uri("" & url_intall & ""), "" & caminho & " \ " & arquivo & "")
+
+    End Sub
+
+    Private Sub webclient2_DownloadProgressChanged(sender As Object, e As DownloadProgressChangedEventArgs) Handles webclient2.DownloadProgressChanged
+
+        If barra.Value <> barra.Maximum Then
+            barra.Maximum = e.TotalBytesToReceive
+            barra.Value = e.BytesReceived
+            lbl_valor.Text = (barra.Maximum)
+
+        Else
+            MsgBox("CONCLUIDO !")
+            barra.Value = 0
+        End If
+
+    End Sub
+
+    Private Sub links_mediworks()
+
+        If lista.SelectedItem = "Mediworks 1" Then
+
+            url_intall = "http://localhost:2605/novo/MediWorks.zip"
+            arquivo = "MediWorks.zip"
+        ElseIf lista.SelectedItem = "Mediworks 2" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediWorks80.7z"
+            arquivo = "MediWorks80.7z"
+        ElseIf lista.SelectedItem = "Mediworks 3" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediWorks80.7z"
+            arquivo = "MediWorks80.7z"
+        ElseIf lista.SelectedItem = "Mediworks 4" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediWorks80.7z"
+            arquivo = "MediWorks80.7z"
+        ElseIf lista.SelectedItem = "Mediworks 5" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediWorks80.7z"
+            arquivo = "MediWorks80.7z"
+        End If
+
+    End Sub
+
+    Private Sub links_medicenter()
+
+        If lista.SelectedItem = "Medicenter 1" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediCenter.7z"
+        ElseIf lista.SelectedItem = "Medicenter 2" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediCenter.7z"
+        ElseIf lista.SelectedItem = "Medicenter 3" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediCenter.7z"
+        ElseIf lista.SelectedItem = "Medicenter 4" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediCenter.7z"
+        ElseIf lista.SelectedItem = "Medicenter 5" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediCenter.7z"
+        End If
+
+
+    End Sub
+
+    Private Sub links_mediremote()
+
+        If lista.SelectedItem = "Mediremote 1" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Mediremote 2" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Mediremote 3" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Mediremote 4" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Mediremote 5" Then
+            url_intall = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        End If
+
+    End Sub
+    Private Sub links_medicapture()
+
+        If lista.SelectedItem = "Medicapture 1" Then
+            capture = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Medicapture 2" Then
+            capture = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Medicapture 3" Then
+            capture = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Medicapture 4" Then
+            capture = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        ElseIf lista.SelectedItem = "Medicapture 5" Then
+            capture = "http://medilab.tecnologia.ws/download/Atualizacao/MediRemote.7z"
+        End If
+
+    End Sub
+
+    Private Sub lista_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lista.SelectedIndexChanged
+        links_mediworks()
+        links_medicenter()
+        links_mediremote()
+        links_medicapture()
+        btn_baixar.Enabled = True
+        'nome_software.Text = lista.SelectedItem.ToString()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        links_mediworks()
+
+        extrair_arquivos()
+
+    End Sub
+
+    Private Sub btn_mediworks_Click(sender As Object, e As EventArgs) Handles btn_mediworks.Click
+        lista.Items.Clear()
+        lista.Items.Add("Mediworks 1")
+        lista.Items.Add("Mediworks 2")
+        lista.Items.Add("Mediworks 3")
+        lista.Items.Add("Mediworks 4")
+        lista.Items.Add("Mediworks 5")
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lista.Items.Clear()
+        lista.Items.Add("Medicenter 1")
+        lista.Items.Add("Medicenter 2")
+        lista.Items.Add("Medicenter 3")
+        lista.Items.Add("Medicenter 4")
+        lista.Items.Add("Medicenter 5")
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        lista.Items.Clear()
+        lista.Items.Add("Mediremote 1")
+        lista.Items.Add("Mediremote 2")
+        lista.Items.Add("Mediremote 3")
+        lista.Items.Add("Mediremote 4")
+        lista.Items.Add("Mediremote 5")
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        lista.Items.Clear()
+        lista.Items.Add("Medicapture 1")
+        lista.Items.Add("Medicapture 2")
+        lista.Items.Add("Medicapture 3")
+        lista.Items.Add("Medicapture 4")
+        lista.Items.Add("Medicapture 5")
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Me.pasta_dialog.ShowDialog()
+        txt_caminho.Text = pasta_dialog.SelectedPath
+
+        If txt_caminho.Text = "" Then
+            btn_baixar.Enabled = False
+        Else
+            btn_baixar.Enabled = True
+
+        End If
+        '' MsgBox(caminho , arquivo"""")
+
+        MsgBox("""" & caminho & "\" & arquivo & """")
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btn_baixar.Enabled = False
+        caminho = txt_caminho.Text
+
+    End Sub
+
+
+    Private Sub extrair_arquivos()
+
+        Dim sc As New Shell32.Shell
+        Dim _pasta As Shell32.Folder = sc.NameSpace("C:\medilab")
+
+        'Dim _arquivo As Shell32.Folder = sc.NameSpace("" & caminho & "\" & arquivo & "")
+
+        Dim _arquivo As Shell32.Folder = sc.NameSpace("C:\medilab\MediWorks.zip")
+
+        _pasta.CopyHere(_arquivo.Items, 4)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Este trecho de código mostra como obter o IP
+        'da máquina local
+
+
+
+            Dim h As IPHostEntry = GetHostEntry(GetHostName)
+            Dim IP As String = CType(h.AddressList.GetValue(0),  _
+              IPAddress).ToString()
+
+        MsgBox("O IP local é: " & IP)
+
+        ' Console.WriteLine("Pressione uma tecla para sair...")
+        'Console.ReadKey()
+        End Sub
+
+
+End Class
